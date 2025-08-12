3dd91f98304391758b478b8a658977e11d3e347b
Dasun Piyumal <w.piyumal2319@gmail.com>
2025-08-12 16:05:41 +0530

    initialize project with Spring Boot, MongoDB, and Kafka dependencies; add Maven wrapper and configuration files

---
+++ src/main/java/com/ticketly/mseventseatingquery/MsEventSeatingQueryApplication.java
@@ -0,0 +1,13 @@
+package com.ticketly.mseventseatingquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEventSeatingQueryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsEventSeatingQueryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ticketly/mseventseatingquery/MsEventSeatingQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketly.mseventseatingquery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEventSeatingQueryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
